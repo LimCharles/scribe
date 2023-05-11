e724f30ca3fb18ad9523c748230f8b78dbeb965e
Charles Lim <charles.andrew.lim@obf.ateneo.edu>
2023-05-11 23:14:19 +0800

    fixed stupid env keys

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    OPEN_AI_KEY: process.env.OPEN_AI_KEY,
+  },
 };
 
 module.exports = nextConfig;
